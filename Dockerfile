9677b46f7e1a526ac3f34901bc45f98355bdcbbb
miyago9267 <miyagomizugen@gmail.com>
2025-03-18 10:09:18 +0800

    feat: init dockerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:18 AS build-stage
+
+WORKDIR /app
+
+COPY package.json yarn.lock ./
+RUN yarn install
+
+COPY . .
+RUN yarn build
+
+FROM node:18-alpine AS production-stage
+
+WORKDIR /app
+
+COPY --from=build-stage /app/.output ./
+COPY --from=build-stage /app/data /app/data
+
+CMD ["node", "server/index.mjs"]
---
+++ docker-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+user="user"
+name="2025-Aprilfool"
+port="2025"
+
+docker build \
+    $@ -t $user/$name:latest . || exit
+[ "$(docker ps | grep $name)" ] && docker kill $name
+[ "$(docker ps -a | grep $name)" ] && docker rm $name
+
+docker run \
+	-itd \
+	-u $(id -u):$(id -g) \
+	--name $name \
+    --network bridge \
+    -p $port:3000 \
+	--restart=always \
+	$user/$name:latest
