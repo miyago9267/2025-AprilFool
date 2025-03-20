7adb3c67d91a908b670acbd35d285b45ab1499c9
miyago9267 <miyagomizugen@gmail.com>
2025-03-20 15:48:54 +0800

    style: 將對話框內縮一點點

--- components/DialogBox.vue
+++ components/DialogBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="dialogue" class="dialog-box z-20 w-4/5 min-h-36 bg-black/70 text-white p-4 rounded-lg cursor-pointer shadow-lg"
+  <div v-if="dialogue" class="dialog-box z-20 w-4/5 min-h-36 bg-black/70 text-white px-8 py-3 rounded-lg cursor-pointer shadow-lg"
        @click="$emit('next')">
     <p v-if="dialogue.character" class="text-2xl font-bold mb-2">{{ dialogue.character }}</p>
     <p class="text-lg">{{ dialogue.text }}</p>
