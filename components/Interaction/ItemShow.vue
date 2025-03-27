e79bb77c08412b611744ecc7d02940f83c388fd8
miyago9267 <miyagomizugen@gmail.com>
2025-03-27 11:43:12 +0800

    style: 優化提示顯示

--- components/Interaction/ItemShow.vue
+++ components/Interaction/ItemShow.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="fixed inset-0 flex items-center justify-center bg-white/80 p-4">
-    <div class="flex flex-col items-center">
+  <div class="fixed inset-0 flex items-center justify-center p-4">
+    <div class="flex flex-col items-center border-2 border-gray-700 rounded-lg p-4 bg-black/40">
       <img
         :src="src"
         :alt="alt"
         class="max-w-full max-h-[40vh] object-contain border-2 border-gray-700 rounded shadow-xl"
       />
-      <p class="mt-2 text-sm text-gray-600" v-if="caption">{{ caption }}</p>
+      <p class="mt-2 text-xl text-gray-200" v-if="caption">{{ caption }}</p>
     </div>
   </div>
 </template>
