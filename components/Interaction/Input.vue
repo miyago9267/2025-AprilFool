081d714cdc10e563528ade34aa4315d227b127c5
miyago9267 <miyagomizugen@gmail.com>
2025-03-31 21:09:26 +0800

    fix: 增加放棄選項

--- components/Interaction/Input.vue
+++ components/Interaction/Input.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="interaction-box absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:w-96 w-[80] p-6 bg-white text-black rounded shadow-lg">
+  <div class="interaction-box absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:w-96 w-[90vw] p-6 bg-white text-black rounded shadow-lg">
     <p class="text-lg font-bold mb-2">{{ interaction.placeholder }}</p>
     <input v-model="userInput" class="border py-3 rounded w-full text-lg text-center" type="text" />
     <button @click="checkAnswer" class="mt-4 bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 w-full">
